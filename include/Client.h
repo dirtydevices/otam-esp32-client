e07735ed4587688d3213dd7d13117344c8db21f3
Tim Wagner <tim@timwagner.me>
2025-07-21 15:32:10 -0400

    -- In progress work

---
+++ include/Client.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "Adapter.h"
+#include "EventEmitter.h"
+#include "Updater.h"
+
+namespace OTAM {
+
+    // Events for an OTA updater
+    enum class ClientEvent : uint8_t {
+        DownloadProgress,  // download percentage complete
+        UpdateComplete,    // true on success, false on failure
+        FlashProgress,     // flashing progress
+        FlashComplete,     // flash complete
+        InitReboot,        // before rebooting the device (this is where the consuming firmware reboots)
+        COUNT
+    };
+
+    class Client : public EventEmitter<ClientEvent, static_cast<std::size_t>(ClientEvent::COUNT)> {
+       public:
+        Client* GetClient(const OtamConfig& config);
+
+        ~Client();
+
+        // initialize the client
+        int8_t init();
+
+        // Check if a firmware update is available
+        int8_t checkFirmwareAvailable(bool& available);
+
+        // download the available firmware update
+        int8_t downloadFirmware();
+
+        // perform the firmware update
+        int8_t updateFirmware();
+
+       private:
+        static Client* _instance;
+
+        Client(const OtamConfig& config);
+
+        Adapter* _adapter;
+        Updater* _updater;
+
+        static void onDownloadProgress(int);
+        static void onFlashProgress(int);
+        static void onUpdateComplete(bool);
+        static void onFlashComplete(bool);
+        static void onInitReboot(void*);
+    };
+}  // namespace OTAM
+
+template <>
+struct EventPayload<OTAM::ClientEvent::DownloadProgress> {
+    using type = int;
+};
+
+template <>
+struct EventPayload<OTAM::ClientEvent::UpdateComplete> {
+    using type = bool;
+};
+
+template <>
+struct EventPayload<OTAM::ClientEvent::FlashProgress> {
+    using type = int;
+};
+
+template <>
+struct EventPayload<OTAM::ClientEvent::FlashComplete> {
+    using type = bool;
+};
+
+template <>
+struct EventPayload<OTAM::ClientEvent::InitReboot> {
+    using type = void*;
+};
